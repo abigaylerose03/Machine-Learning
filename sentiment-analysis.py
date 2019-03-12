8a35772e2e3ec9c9c169f6d13d358517c0615551
Abigayle Peterson <32112798+abigaylerose03@users.noreply.github.com>
2019-03-11 22:59:39 -0700

    it works wow

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -4,11 +4,16 @@ from sklearn.ensemble import RandomForestClassifier
 from KaggleWord2VecUtility import KaggleWord2VecUtility
 import pandas as pd
 import numpy
+import logging
 
 from nltk.corpus import stopwords # imports various modules for string cleaning
 import re
 import nltk.data
-from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup # removes stopwords
+from gensim.models import Word2Vec
+
+from gensim import models
 
 # nltk.download()
 
@@ -34,13 +39,13 @@ def review_to_wordlist(review, remove_stopwords=False ):
     # Removes HTML
     review_text = BeautifulSoup(review).get_text()
     # Removes non-letters
-    review_text = re.sub("[^a-zA-Z]","", review_text)
+    review_text = re.sub("[^a-zA-Z]"," ", review_text)
     # Converts words to lower case and splits them by whitecase to an array
     words = review_text.lower().split()
     # Optionally remove stop words (false by default)
     if remove_stopwords:
         stops = set(stopwords.words("english"))
-        words = [w for w in words if not w in words]
+        words = [w for w in words if not w in stops]
     # Returns the list of words
     return(words)
 
@@ -51,11 +56,12 @@ tokenizer = nltk.data.load('tokenizers/punkt/english.pickle');
 # splits a reiew into parsed sentences. Returns a list of sentences, where each
 # sentence is a list of words
 def review_to_sentences(review, tokenizer, remove_stopwords=False ):
-    raw_sentences = tokenizer.tokenize(review.strip())
+    raw_sentences = tokenizer.tokenize(review.strip().decode('utf-8'))
     sentences = []
     for raw_sentence in raw_sentences:
         if len(raw_sentence) > 0:
-            sentences.append(review_to_wordlist( raw_sentence, remove_stopwords ))
+            sentences.append(review_to_wordlist(raw_sentence, \
+             remove_stopwords ))
     return sentences
 
 sentences = [] # initialize an empty list of sentences for the review_to_sentences()
@@ -67,7 +73,35 @@ for review in train["review"]:
 print("Parsing sentences from unlabled set")
 for review in unlabeled_train["review"]:
     sentences += review_to_sentences(review, tokenizer)
+
 print(len(sentences))
-print(sentences[0])
+
+# Training Model
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s' , \
+	level = logging.INFO);
+
+num_features = 360 # word vector dimesionality
+min_word_count = 40 # minimum word count
+num_workers = 4 # number of threads to run in parallel
+context = 10 # context window size
+downsampling = 1e-3 # downsample setting for frequent words
+
+print("Training model...");
+
+model = Word2Vec(sentences, workers = num_workers, \
+	size = num_features, min_count = min_word_count, \
+	window = context, sample = downsampling)
+
+model.init_sims(replace=True)
+model_name = "300features_40minwords_10context"
+model.save(model_name)
+
+# Word2Vec.load("testData.tsv")
+
+print(model.doesnt_match("man woman child kitchen".split()))
+print(model.most_similar("man"))
+print(model.most_similar("computer"))
+
 
 
